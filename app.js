c7500812f53f4725277da4fc05a569c6282f76d9
keyshanavii <ayassikeysha@gmail.com>
2024-10-24 13:51:07 +0700

    selesai

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
+  const tinggiBadan = ["150", "155", "148", "165","170", "168", "166"]
+  const namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
 
+  for (let i = 0; i < namaAtlet.length; i++){
+    console.log(`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiBadan[i]}`)
+  }
 
 
 
@@ -9,5 +14,6 @@ function runner() {
 
   /// JANGAN UBAH SELAINNYA
 }
+runner()
 
 module.exports = runner
